1806b27995952bf2cae465e0e923de7e82f2814e
MaxOsef <leber.maximilien@gmail.com>
2019-01-30 00:20:16 +0100

    First login window

--- main.c
+++ main.c
@@ -3,15 +3,18 @@
 #include <ncurses.h>
 
 
+WINDOW *create_newwin(int height, int width, int starty, int startx);
 char * intprtkey(int ch);
+void destroy_win(WINDOW *local_win);
 
 
 int main(void) {
 
     WINDOW * mainwin;
+    WINDOW * loginwin;
+	int startx, starty, width, height;
     int ch;
-
-
+	
     /*  Initialize ncurses  */
 
     if ( (mainwin = initscr()) == NULL ) {
@@ -20,31 +23,37 @@ int main(void) {
     }
 
     noecho();                  /*  Turn off key echoing                 */
-    keypad(mainwin, TRUE);     /*  Enable the keypad for non-char keys  */
+    cbreak();
+	keypad(mainwin, TRUE);     /*  Enable the keypad for non-char keys  */
+	start_color();
 
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	height = 10;
+	width = 30;
+	starty = (LINES - height) / 2;	/* Calculating for a center placement */
+	startx = (COLS - width) / 2;	/* of the window		*/
 
     /*  Print a prompt and refresh() the screen  */
-
-    mvaddstr(5, 10, "Press a key ('q' to quit)...");
-    mvprintw(7, 10, "You pressed: ");
-    refresh();
-
-
-    /*  Loop until user presses 'q'  */
-
-    while ( (ch = getch()) != 'q' ) {
-
-	/*  Delete the old response line, and print a new one  */
-
-	deleteln();
-	mvprintw(7, 10, "You pressed: 0x%x (%s)", ch, intprtkey(ch));
+    
+	attron(COLOR_PAIR(1));
+	printw("Press F1 to exit");
 	refresh();
+	attroff(COLOR_PAIR(1));
+
+	loginwin = create_newwin(height, width, starty, startx);
+
+    /*  Loop until user presses 'F1'  */
+    while ( (ch = getch()) != KEY_F(1) ) {
+
+		/*  Delete the win, and print a new one  */
+		destroy_win(loginwin);
+		loginwin = create_newwin(height, width, starty, startx);
+		refresh();
     }
 
-
     /*  Clean up after ourselves  */
-
     delwin(mainwin);
+    delwin(loginwin);
     endwin();
     refresh();
 
@@ -53,7 +62,6 @@ int main(void) {
 
 
 /*  Struct to hold keycode/keyname information  */
-
 struct keydesc {
     int  code;
     char name[20];
@@ -61,11 +69,13 @@ struct keydesc {
 
 
 /*  Returns a string describing a character passed to it  */
-
 char * intprtkey(int ch) {
 
-    /*  Define a selection of keys we will handle  */
-
+    /*  Define a selection of keys we will handle
+     *
+     *\/!\ C'est pas inutile de garder tout ca? On a pas vrmt besoin
+           d'afficher tout ces caractère a l'écran de toute facon, 
+           jpense pas que tout soit nécessaire \/!\  */
     static struct keydesc keys[] = { { KEY_UP,        "Up arrow"        },
 				     { KEY_DOWN,      "Down arrow"      },
 				     { KEY_LEFT,      "Left arrow"      },
@@ -118,3 +128,36 @@ char * intprtkey(int ch) {
     return NULL;        /*  We shouldn't get here  */
 }
 
+WINDOW *create_newwin(int height, int width, int starty, int startx)
+{	WINDOW *local_win;
+
+	local_win = newwin(height, width, starty, startx);
+	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
+					 * for the vertical and horizontal
+					 * lines			*/
+	wrefresh(local_win);		/* Show that box 		*/
+
+	return local_win;
+}
+
+void destroy_win(WINDOW *local_win)
+{	
+	/* box(local_win, ' ', ' '); : This won't produce the desired
+	 * result of erasing the window. It will leave it's four corners 
+	 * and so an ugly remnant of window. 
+	 */
+	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	/* The parameters taken are 
+	 * 1. win: the window on which to operate
+	 * 2. ls: character to be used for the left side of the window 
+	 * 3. rs: character to be used for the right side of the window 
+	 * 4. ts: character to be used for the top side of the window 
+	 * 5. bs: character to be used for the bottom side of the window 
+	 * 6. tl: character to be used for the top left corner of the window 
+	 * 7. tr: character to be used for the top right corner of the window 
+	 * 8. bl: character to be used for the bottom left corner of the window 
+	 * 9. br: character to be used for the bottom right corner of the window
+	 */
+	wrefresh(local_win);
+	delwin(local_win);
+}
