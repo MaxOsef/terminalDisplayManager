32ba76853808a9386f678ae6205b31997f0dd794
MaxOsef <leber.maximilien@gmail.com>
2019-02-09 20:21:19 +0100

    New test Pam, trying another way

--- main.c
+++ main.c
@@ -13,14 +13,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define SERVICE_NAME "display_manager"
+#define SERVICE_NAME "checkuser"
 
 #define APP "STDM"
 #define VERSION "v0.0.1"
 #define N_FIELDS 7
 #define HEIGHT 10
 #define WIDTH 60
-// #define KEY_ENTER 65293
 
 #define err(name)                                   \
     do {                                            \
@@ -30,26 +29,31 @@
         return false;                               \
     } while (1);                                    \
 
+
+/* ------------------ A REVOIR !!! --------------------------------------------- */
+
+// static int conv(int num_msg, const struct pam_message **msg,
+//                 struct pam_response **resp, void *appdata_ptr);
+
+// static pam_handle_t *pam_handle;
+// bool login(char *username, char *userpw, char *userdesk, pid_t *child_pid);
+// static void login_func(char *username, char *userpw, char *userdesk);
+// static char loaduser(void);
+
+/* -------------------------------------------------------------------------------*/
+
+
+
+
 static FORM *form;
 static FIELD *fields[N_FIELDS];
 static WINDOW *win_body, *win_form;
 
-bool login(char *username, char *userpw, char *userdesk, pid_t *child_pid);
-bool logout(void);
-
-static void login_func(char *username, char *userpw, char *userdesk);
+// bool logout(void);
 static void driver(char *username, char *userpw, char *userdesk, int ch);
-// static char loaduser(void);
-
-static void init_env(struct passwd *pw);
-static void set_env(char *name, char *value);
-static int end(int last_result);
-
-static int conv(int num_msg, const struct pam_message **msg,
-                struct pam_response **resp, void *appdata_ptr);
-
-static pam_handle_t *pam_handle;
-
+// static void init_env(struct passwd *pw);
+// static void set_env(char *name, char *value);
+// static int end(int last_result);
 static char* trim_whitespaces(char *str);
 
 int main(void)
@@ -204,7 +208,7 @@ static void driver(char *username, char *userpw, char *userdesk, int ch)
             break;
 		case KEY_ENTER:
 		case '\n':
-			login_func(username, userpw, userdesk);
+// 			login_func(username, userpw, userdesk);
 			break;
         default:
             // Print normal charaters
@@ -215,170 +219,54 @@ static void driver(char *username, char *userpw, char *userdesk, int ch)
     wrefresh(win_form);
 }
 
-static void login_func(char *username, char *userpw, char *userdesk) {
-	
-    pid_t child_pid;
-    if (login(username, userpw, userdesk, &child_pid)) {
-		addstr("Shelling out...");
-    	def_prog_mode();           /* save current tty modes */
-    	endwin();                  /* restore original tty modes */
-    	system("zsh");              /* run shell */
-	
-        // Wait for child process to finish (wait for logout)
-        int status;
-        waitpid(child_pid, &status, 0);
 
-    	addstr("returned.\n");     /* prepare return message */
-    	refresh();                 /* restore save modes, repaint screen */
+// static void init_env(struct passwd *pw) {
+//     set_env("HOME", pw->pw_dir);
+//     set_env("PWD", pw->pw_dir);
+//     set_env("SHELL", pw->pw_shell);
+//     set_env("USER", pw->pw_name);
+//     set_env("LOGNAME", pw->pw_name);
+//     set_env("PATH", "/usr/local/sbin:/usr/local/bin:/usr/bin");
+//     set_env("MAIL", _PATH_MAILDIR);
+//     putenv("TERM=$TERM");
+// 
+//     size_t xauthority_len = strlen(pw->pw_dir) + strlen("/.Xauthority") + 1;
+//     char *xauthority = malloc(xauthority_len);
+//     snprintf(xauthority, xauthority_len, "%s/.Xauthority", pw->pw_dir);
+//     set_env("XAUTHORITY", xauthority);
+//     free(xauthority);
+// }
+// 
+// static void set_env(char *name, char *value) {
+//     // The `+ 2` is for the '=' and the null byte
+//     size_t name_value_len = strlen(name) + strlen(value) + 2;
+//     char *name_value = malloc(name_value_len);
+//     snprintf(name_value, name_value_len,  "%s=%s", name, value);
+//     pam_putenv(pam_handle, name_value);
+//     free(name_value);
+// }
 
-        logout();
-    } else {
-		mvaddstr(5, 5, "Error while login in...");
-    }
-
-	/*  Attention !! 
-		Il faut penser à vider le 
-		champ userpw avant de revenir 
-		dans ncurses	*/
-}
-
-bool login(char *username, char *userpw, char *userdesk, pid_t *child_pid) {
-    const char *data[2] = {username, userpw};
-    struct pam_conv pam_conv = {
-        conv, data
-    };
-
-    int result = pam_start(SERVICE_NAME, username, &pam_conv, &pam_handle);
-    if (result != PAM_SUCCESS) {
-        err("pam_start");
-    }
-
-    result = pam_authenticate(pam_handle, 0);
-    if (result != PAM_SUCCESS) {
-        err("pam_authenticate");
-    }
-
-    result = pam_acct_mgmt(pam_handle, 0);
-    if (result != PAM_SUCCESS) {
-        err("pam_acct_mgmt");
-    }
-
-    result = pam_setcred(pam_handle, PAM_ESTABLISH_CRED);
-    if (result != PAM_SUCCESS) {
-        err("pam_setcred");
-    }
-
-    result = pam_open_session(pam_handle, 0);
-    if (result != PAM_SUCCESS) {
-        pam_setcred(pam_handle, PAM_DELETE_CRED);
-        err("pam_open_session");
-    }
-
-    struct passwd *pw = getpwnam(username);
-    init_env(pw);
-
-    *child_pid = fork();
-    if (*child_pid == 0) {
-        chdir(pw->pw_dir);
-        // We don't use ~/.xinitrc because we should already be in the users home directory
-        char *cmd = "exec /bin/zsh --login .xinitrc";
-        execl(pw->pw_shell, pw->pw_shell, "-c", cmd, NULL);
-        printf("Failed to start window manager");
-        exit(1);
-    }
-
-    return true;
-}
-
-static int conv(int num_msg, const struct pam_message **msg,
-                struct pam_response **resp, void *appdata_ptr) {
-    int i;
-
-    *resp = calloc(num_msg, sizeof(struct pam_response));
-    if (*resp == NULL) {
-        return PAM_BUF_ERR;
-    }
-
-    int result = PAM_SUCCESS;
-    for (i = 0; i < num_msg; i++) {
-        char *username, *userpw;
-        switch (msg[i]->msg_style) {
-        case PAM_PROMPT_ECHO_ON:
-            username = ((char **) appdata_ptr)[0];
-            (*resp)[i].resp = strdup(username);
-            break;
-        case PAM_PROMPT_ECHO_OFF:
-            userpw = ((char **) appdata_ptr)[1];
-            (*resp)[i].resp = strdup(userpw);
-            break;
-        case PAM_ERROR_MSG:
-            fprintf(stderr, "%s\n", msg[i]->msg);
-            result = PAM_CONV_ERR;
-            break;
-        case PAM_TEXT_INFO:
-            printf("%s\n", msg[i]->msg);
-            break;
-        }
-        if (result != PAM_SUCCESS) {
-            break;
-        }
-    }
-
-    if (result != PAM_SUCCESS) {
-        free(*resp);
-        *resp = 0;
-    }
-
-    return result;
-}
-
-static void init_env(struct passwd *pw) {
-    set_env("HOME", pw->pw_dir);
-    set_env("PWD", pw->pw_dir);
-    set_env("SHELL", pw->pw_shell);
-    set_env("USER", pw->pw_name);
-    set_env("LOGNAME", pw->pw_name);
-    set_env("PATH", "/usr/local/sbin:/usr/local/bin:/usr/bin");
-    set_env("MAIL", _PATH_MAILDIR);
-    putenv("TERM=xterm");
-
-    size_t xauthority_len = strlen(pw->pw_dir) + strlen("/.Xauthority") + 1;
-    char *xauthority = malloc(xauthority_len);
-    snprintf(xauthority, xauthority_len, "%s/.Xauthority", pw->pw_dir);
-    set_env("XAUTHORITY", xauthority);
-    free(xauthority);
-}
-
-static void set_env(char *name, char *value) {
-    // The `+ 2` is for the '=' and the null byte
-    size_t name_value_len = strlen(name) + strlen(value) + 2;
-    char *name_value = malloc(name_value_len);
-    snprintf(name_value, name_value_len,  "%s=%s", name, value);
-    pam_putenv(pam_handle, name_value);
-    free(name_value);
-}
-
-bool logout(void) {
-    int result = pam_close_session(pam_handle, 0);
-    if (result != PAM_SUCCESS) {
-        pam_setcred(pam_handle, PAM_DELETE_CRED);
-        err("pam_close_session");
-    }
-
-    result = pam_setcred(pam_handle, PAM_DELETE_CRED);
-    if (result != PAM_SUCCESS) {
-        err("pam_setcred");
-    }
-
-    end(result);
-    return true;
-}
-
-static int end(int last_result) {
-    int result = pam_end(pam_handle, last_result);
-    pam_handle = 0;
-    return result;
-}
+// bool logout(void) {
+//     int result = pam_close_session(pam_handle, 0);
+//     if (result != PAM_SUCCESS) {
+//         pam_setcred(pam_handle, PAM_DELETE_CRED);
+//         err("pam_close_session");
+//     }
+// 
+//     result = pam_setcred(pam_handle, PAM_DELETE_CRED);
+//     if (result != PAM_SUCCESS) {
+//         err("pam_setcred");
+//     }
+// 
+//     end(result);
+//     return true;
+// }
+// 
+// static int end(int last_result) {
+//     int result = pam_end(pam_handle, last_result);
+//     pam_handle = 0;
+//     return result;
+// }
  
 static char* trim_whitespaces(char *str)
 {
@@ -403,3 +291,147 @@ static char* trim_whitespaces(char *str)
 	return str;
 }
 
+
+
+/* ------------------ A REVOIR !!! --------------------------------------------- */
+
+// static void login_func(char *username, char *userpw, char *userdesk) {
+// 	
+//     pid_t child_pid;
+//     if (login(username, userpw, userdesk, &child_pid)) {
+// 		addstr("Shelling out...");
+//     	def_prog_mode();           /* save current tty modes */
+//     	endwin();                  /* restore original tty modes */
+//     	system("zsh");              /* run shell */
+// 	
+//         // Wait for child process to finish (wait for logout)
+//         int status;
+//         waitpid(child_pid, &status, 0);
+// 
+//     	addstr("returned.\n");     /* prepare return message */
+//     	refresh();                 /* restore save modes, repaint screen */
+// 
+//         logout();
+//     } else {
+// 		mvaddstr(5, 5, "Error while login in...");
+//     }
+// 
+// 	/*  Attention !! 
+// 		Il faut penser à vider le 
+// 		champ userpw avant de revenir 
+// 		dans ncurses	*/
+// }
+
+// bool login(char *username, char *userpw, char *userdesk, pid_t *child_pid) {
+//     const char *data[2] = {username, userpw};
+//     struct pam_conv pam_conv = {
+//         conv, data
+//     };
+// 
+//     const void **user;
+// 
+// //     char pam_user[32], pam_service[32], pam_authtok[32], pam_tty[32], pam_user_prompt[32];
+// 
+// //     pam_user = pam_get_item(pam_handle, PAM_USER, user);
+// //     pam_service = pam_get_item(pam_handle, PAM_SERVICE, user);
+// //     pam_authtok = pam_get_item(pam_handle, PAM_AUTHTOK, user);
+// //     pam_tty = pam_get_item(pam_handle, PAM_TTY, user);
+// //     pam_user_prompt = pam_get_item(pam_handle, PAM_USER_PROMPT, user);
+// 
+// 
+// //     mvprintw(LINES-6, 1, "%s", pam_get_item(pam_handle, PAM_USER, user));
+// //     mvprintw(LINES-5, 1, "%s", pam_service);
+// //     mvprintw(LINES-4, 1, "%s", pam_authtok);
+// //     mvprintw(LINES-3, 1, "%s", pam_tty);
+// //     mvprintw(LINES-3, 1, "%s", pam_tty);
+// 
+//     int result = pam_start(SERVICE_NAME, username, &pam_conv, &pam_handle);
+//     if (result != PAM_SUCCESS) {
+//         err("pam_start");
+//     }
+// 
+//     result = pam_authenticate(pam_handle, 0);
+//     if (result != PAM_SUCCESS) { 
+//         err("pam_authenticate");
+//     }
+// 
+// 
+//         mvprintw(LINES-2, 1, "%s", username);
+//         mvprintw(LINES-1, 1, "%s", userpw);
+//         getch();
+// 
+//     result = pam_acct_mgmt(pam_handle, 0);
+//     if (result != PAM_SUCCESS) {
+//         err("pam_acct_mgmt");
+//     }
+// 
+//     result = pam_setcred(pam_handle, PAM_ESTABLISH_CRED);
+//     if (result != PAM_SUCCESS) {
+//         err("pam_setcred");
+//     }
+// 
+//     result = pam_open_session(pam_handle, 0);
+//     if (result != PAM_SUCCESS) {
+//         pam_setcred(pam_handle, PAM_DELETE_CRED);
+//         err("pam_open_session");
+//     }
+// 
+//     struct passwd *pw = getpwnam(username);
+//     init_env(pw);
+// 
+//     *child_pid = fork();
+//     if (*child_pid == 0) {
+//         chdir(pw->pw_dir);
+//         // We don't use ~/.xinitrc because we should already be in the users home directory
+//         char *cmd = "exec /bin/zsh --login";
+//         execl(pw->pw_shell, pw->pw_shell, "-c", cmd, NULL);
+//         printf("Failed to start window manager");
+//         exit(1);
+//     }
+// 
+//     return true;
+// }
+// 
+// static int conv(int num_msg, const struct pam_message **msg,
+//                 struct pam_response **resp, void *appdata_ptr) {
+//     int i;
+// 
+//     *resp = calloc(num_msg, sizeof(struct pam_response));
+//     if (*resp == NULL) {
+//         return PAM_BUF_ERR;
+//     }
+// 
+//     int result = PAM_SUCCESS;
+//     for (i = 0; i < num_msg; i++) {
+//         char *username, *userpw;
+//         switch (msg[i]->msg_style) {
+//         case PAM_PROMPT_ECHO_ON:
+//             username = ((char **) appdata_ptr)[0];
+//             (*resp)[i].resp = strdup(username);
+//             break;
+//         case PAM_PROMPT_ECHO_OFF:
+//             userpw = ((char **) appdata_ptr)[1];
+//             (*resp)[i].resp = strdup(userpw);
+//             break;
+//         case PAM_ERROR_MSG:
+//             fprintf(stderr, "%s\n", msg[i]->msg);
+//             result = PAM_CONV_ERR;
+//             break;
+//         case PAM_TEXT_INFO:
+//             printf("%s\n", msg[i]->msg);
+//             break;
+//         }
+//         if (result != PAM_SUCCESS) {
+//             break;
+//         }
+//     }
+// 
+//     if (result != PAM_SUCCESS) {
+//         free(*resp);
+//         *resp = 0;
+//     }
+// 
+//     return result;
+// }
+
+/* -------------------------------------------------------------------------------*/
